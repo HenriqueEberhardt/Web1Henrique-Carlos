92e7fb2715af53dfec31f51e504f0e7a2ee14623
carlosisenseee <carlosalfredoisensee@gmail.com>
2026-04-05 20:37:43 -0300

    Primeiro Commit, subindo telad e login com html, js e css prontos

---
+++ script.js
@@ -0,0 +1,37 @@
+// Tela de Login
+const form = document.getElementById("loginForm");
+
+window.onload = () => {
+    if (localStorage.getItem("lembrar") === "true") {
+        document.getElementById("usuario").value = localStorage.getItem("usuario");
+        document.getElementById("lembrar").checked = true;
+    }
+};
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let user = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    if (user === "" || senha === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    sessionStorage.setItem("usuarioLogado", user.split(".")[0].toUpperCase() + " " +
+        user.split(".")[1].toUpperCase());
+
+    if (document.getElementById("lembrar").checked) {
+        localStorage.setItem("lembrar", true);
+        localStorage.setItem("usuario", user);
+    } else {
+        localStorage.setItem("lembrar", false);
+    }
+
+    window.location.href = "index.html";
+});
+
+function recuperarSenha() {
+    alert("Teste troca de senha");
+}
